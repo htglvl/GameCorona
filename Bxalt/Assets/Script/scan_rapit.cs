49537613954195d2e91b540a7f965362d84639df
htglvl <50829560+htglvl@users.noreply.github.com>
2020-06-16 19:27:07 +0700

    thay đổi graphic, map, enemy, thêm A*, chưa làm AI
    thêm map = fang auto tilling, dùng grid làm map

---
+++ Bxalt/Assets/Script/scan_rapit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Pathfinding;
+
+public class scan_rapit : MonoBehaviour
+{
+    public float rerate;
+    private void Start()
+    {
+        InvokeRepeating("RS", 1f, rerate);
+    }
+    private void RS()
+    {
+        AstarPath.active.Scan();
+    }
+}
---
+++ Bxalt/Assets/iconDisplayWhenPlay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iconDisplayWhenPlay : MonoBehaviour
+{
+    public GameObject ObjectToSetActiveOnStart;
+    void Start()
+    {
+        ObjectToSetActiveOnStart.SetActive(true);
+    }
+}
