15eb8f7c54ea1d37e2d9e0b6f0d11e20b5a2f83c
htglvl <50829560+htglvl@users.noreply.github.com>
2020-06-17 21:25:52 +0700

    đang làm dở waypoint for AI

---
+++ Bxalt/Assets/Editor/WayPointManagerWindows.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class WayPointManagerWindows : EditorWindow
+{
+    [MenuItem("Tools/WayPoint Editor")]
+    public static void Open()
+    {
+        GetWindow<WayPointManagerWindows>();
+    }
+    public Transform waypointRoot;
+    private void OnGUI()
+    {
+        SerializedObject obj = new SerializedObject(this);
+        EditorGUILayout.PropertyField(obj.FindProperty("waypointRoot"));
+
+        if (waypointRoot == null)
+        {
+            EditorGUILayout.HelpBox("Root Transform must be selected. Assign a root transform now", MessageType.Warning);
+        }
+        else
+        {
+            EditorGUILayout.BeginVertical("box");
+            DrawButton();
+            EditorGUILayout.EndVertical();
+        }
+
+        obj.ApplyModifiedProperties();
+    }
+    void DrawButton()
+    {
+        if (GUILayout.Button("Creat Waypoint"))
+        {
+            CreatWayPoint();
+        }
+    }
+    public void CreatWayPoint()
+    {
+        GameObject wayPointObj = new GameObject("Waypoint " + waypointRoot.childCount, typeof(WayPoint));
+        wayPointObj.transform.SetParent(waypointRoot, false);
+        WayPoint wayPoint = wayPointObj.GetComponent<WayPoint>();
+        if (waypointRoot.childCount > 1)
+        {
+
+        }
+    }
+}
+
---
+++ Bxalt/Assets/Scripts/WayPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    public WayPoint previousWaypoint;
+    public WayPoint nextWaypoint;
+
+    [Range(0f, 5f)]
+    public float width = 1f;
+
+    public Vector3 GetPosition()
+    {
+        Vector3 minBound = transform.position + transform.right * width / 2;
+        Vector3 maxBound = transform.position - transform.right * width / 2;
+        return Vector3.Lerp(minBound, maxBound, Random.Range(0f, 1f));
+    }
+}
